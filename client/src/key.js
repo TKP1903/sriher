bceea76f388c78c577a717909651a90d1dc1e364
gsksudheer <sudheer.gsk77@gmail.com>
2022-09-28 13:00:20 +0530

    add react-scripts

--- client/src/key.js
+++ client/src/key.js
@@ -1,8 +1,8 @@
 export const API_URL = window.location.hostname.includes("localhost")
-  ? "http://localhost:4000"
-  : "/node";
+  ? "http://65.0.122.68:4000"
+  : "http://65.0.122.68:4000/opai";
 
 export const CLIENT_URL = window.location.hostname.includes("localhost")
-  ? "http://localhost:3000"
-  : "/";
+  ? "http://65.0.122.68:3000/"
+  : "http://65.0.122.68:3000/";
 
