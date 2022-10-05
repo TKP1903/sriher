ce07caa17898bc072454c0178c06a0f9c645efd2
gsksudheer <sudheer.gsk77@gmail.com>
2022-10-05 11:29:26 +0530

    render completed events

--- client/src/components/Events/completedEvents.jsx
+++ client/src/components/Events/completedEvents.jsx
@@ -10,37 +10,48 @@ const CompletedEvents = () => {
   useEffect(() => {
     reduxState?.events && setEventsData(reduxState.events?.events);
   }, [reduxState?.events]);
-
+  var cmpEventsCount = 0;
+  eventData?.map((item) => {
+    if (item.status !== "active") cmpEventsCount = cmpEventsCount + 1;
+  });
   return (
     <>
-      <h1 className="text-3xl font-bold mt-3 text-center p-2 w-full bg-gray-100">
-        Completed Events
-      </h1>
-      <div className="flex flex-wrap mx-6 md:mx-32 items-center justify-between gap-8">
-        {eventData?.length > 0 ? (
-          eventData?.map(
-            (data) =>
-              data.status !== "active" && (
-                <div className="w-full md:w-72 border border-gray-200 py-2 flex flex-col items-center justify-center gap-2 shadow-2xl bg-grey-500">
-                  <div className="w-full md:w-64 h-80">
-                    <img
-                      src={`${data.image}`}
-                      alt=""
-                      className="w-full h-full"
-                    />
-                  </div>
-                  <div className="flex flex-col items-center">
-                    <h2 className="text-xl font-bold">{data.eventName}</h2>
-                    <p className="w-full px-3 py-2 text-sm">
-                      {data.description}
-                    </p>
-                  </div>
+      <div>
+        {cmpEventsCount > 1 && (
+          <div>
+            <h1 className="text-3xl font-bold mt-3 text-center p-2 w-full bg-gray-100">
+              Completed Events
+            </h1>
+            <div className="flex flex-wrap mx-6 md:mx-32 items-center justify-between gap-8">
+              {eventData?.length > 0 ? (
+                eventData?.map(
+                  (data) =>
+                    data.status !== "active" && (
+                      <div className="w-full md:w-72 border border-gray-200 py-2 flex flex-col items-center justify-center gap-2 shadow-2xl bg-grey-500">
+                        <div className="w-full md:w-64 h-80">
+                          <img
+                            src={`${data.image}`}
+                            alt=""
+                            className="w-full h-full"
+                          />
+                        </div>
+                        <div className="flex flex-col items-center">
+                          <h2 className="text-xl font-bold">
+                            {data.eventName}
+                          </h2>
+                          <p className="w-full px-3 py-2 text-sm">
+                            {data.description}
+                          </p>
+                        </div>
+                      </div>
+                    )
+                )
+              ) : (
+                <div className="w-full h-screen flex items-center justify-center">
+                  <MoonLoader width={1000} height={1000} />
                 </div>
-              )
-          )
-        ) : (
-          <div className="w-full h-screen flex items-center justify-center">
-            <MoonLoader width={1000} height={1000} />
+              )}
+            </div>
           </div>
         )}
       </div>
